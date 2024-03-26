2f044c97c15a7604a863182ce1ef6ea9177bad1c
kaden adkins <amusableblackice@gmail.com>
2024-03-26 01:34:01 -0500

    I finally figured it out and im getting more comfortable with lists sorry it took so long :)

--- finite.py
+++ finite.py
@@ -1,20 +1,28 @@
 from llist import LList, Node, append
+import random
 
 
-def length(lst):
+def llength(lst):
     """return the length of a finite linked list"""
-    pass  # delete this line when you add your code
-
+    count = 0
+    cnode = lst.head
+    while cnode:
+        count += 1
+        cnode = cnode.next
+    return count
 
 def llprint(lst):
     """print a finite linked list"""
-    pass  # delete this line when you add your code
-
+    cnode = lst.head
+    while cnode:
+        print(cnode.val, end=" ")
+        cnode = cnode.next
+    
 
 if __name__ == "__main__":
+    llist = LList()
+    for i in range(10):
+        append(llist,(Node(random.randint(0,100))))
 
-    pass  # delete this line when you add your code below
-
-    #
-    # your tests go here
-    #
+from genfinite import lst
+print(llength(lst))
